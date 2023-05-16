60dc65800b98e810cd9724cccbe53cfcd6c92b04
Ojas Mittal <ojasfarm31@gmail.com>
2023-05-16 23:02:42 +0530

    initial commit.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ Stock_Market = {'Year': [2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,
                 'Stock_Index_Price': [1464,1394,1357,1293,1256,1254,1234,1195,1159,1167,1130,1075,1047,965,943,958,971,949,884,866,876,822,704,719]
                 }
 
+
+
 df = pd.DataFrame(Stock_Market,columns = ['Year','Month','EconomicGrowth_Rate','Unemployment_Rate','Stock_Index_Price'])
 x = df[['EconomicGrowth_Rate','Unemployment_Rate']]
 y = df['Stock_Index_Price']
